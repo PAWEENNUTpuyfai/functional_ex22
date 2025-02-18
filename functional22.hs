78a82e9d51dd18f3dfb3f6951bdfc9ad0eee356f
PAWEENNUTpuyfai <110964154+PAWEENNUTpuyfai@users.noreply.github.com>
2025-02-18 12:11:53 +0700

    Update functional22.hs

--- functional22.hs
+++ functional22.hs
@@ -56,75 +56,63 @@ instance Monad m => Monad (EitherT e m) where
 
 --implement a monad transformer for the arrow monad:
 --ContT r m a, wrapping m (r -> a)
-
-
 newtype ContT r m a =
-  ContT { runContT :: (a -> m r) -> m r }
+  ContT { runContT :: m (r -> a) }
 
 instance MonadTrans (ContT r) where
   lift :: Monad m => m a -> ContT r m a
-  lift ma = ContT $ \k -> ma >>= k
+  lift = ContT . fmap (const <$> id)
 
 instance Functor m => Functor (ContT r m) where
-  fmap f (ContT x) = ContT $ \k -> x (k . f)
+  fmap f (ContT x) = ContT $ fmap (f .) x
 
 instance Monad m => Applicative (ContT r m) where
-  pure x = ContT ($ x)
-  (ContT mf) <*> (ContT mx) = ContT $ \k ->
-    mf (\f -> mx (k . f))
+  pure x = ContT $ pure (const x)
+  (ContT mf) <*> (ContT mx) = ContT $ do
+    f <- mf
+    x <- mx
+    return $ \r -> f r (x r)
 
 instance Monad m => Monad (ContT r m) where
-  (ContT mx) >>= f = ContT $ \k ->
-    mx (\x -> runContT (f x) k)
+  (ContT mx) >>= f = ContT $ do
+    x <- mx
+    return $ \r -> runContT (f (x r)) r
 
 --modify the signup page example to use EitherT transformer
 --be sure to give helpful error messages
 
-readEmail :: IO (Maybe String)
+type Error = String
+
+readEmail :: IO (Either Error String)
 readEmail = do
   putStrLn "Please enter your email!"
   str <- getLine
   if '@' `elem` str && '.' `elem` str
-    then return $ Just str
-    else return Nothing
+    then return $ Right str
+    else return $ Left "email must contain '@' and '.'"
 
-readPassword :: IO (Maybe String)
+readPassword :: IO (Either Error String)
 readPassword = do
   putStrLn "Please enter your Password!"
   str <- getLine
-  if length str < 8 || null (filter isUpper str)
-      || null (filter isLower str)
-    then return Nothing
-    else return $ Just str
-
-type Error = String
-
-readEmail' :: EitherT Error IO String
-readEmail' = EitherT $ do
-    email <- readEmail 
-    return $ case email of
-        Just e  -> Right e
-        Nothing -> Left "Invalid email"
-
-readPassword' :: EitherT Error IO String
-readPassword' = EitherT $ do
-    password <- readPassword  
-    return $ case password of
-        Just p  -> Right p
-        Nothing -> Left "password "
+  case () of
+    _ | length str < 8 -> return $ Left "Password must contain at least 8 characters"
+      | not (any isUpper str) -> return $ Left "Password must contain uppercase letter"
+      | not (any isLower str) -> return $ Left "Password must contain lowercase letter"
+      | otherwise -> return $ Right str
 
 signup' :: EitherT Error IO (String, String)
 signup' = do
-    email <- readEmail'
-    password <- readPassword'
-    password2 <- readPassword'
+    email <- EitherT readEmail
+    password <- EitherT readPassword
+    password2 <- EitherT readPassword
     if password == password2
         then return (email, password)
-        else EitherT . return $ Left "Passwords do not match"
+        else EitherT . return $ Left "Passwords do not match"  
 
 main :: IO ()
 main = do
     signupRes <- runEitherT signup'
     case signupRes of
-        Left err  -> putStrLn $ "Signup failed: " ++ err
+        Left err  -> putStrLn $ "Signup failed " ++ err
         Right _   -> putStrLn "Signup success"
